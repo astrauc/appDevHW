d6c55034ff09328f4edc5602b578d71c1bb44a76
Anson <a1strauch@yahoo.com>
2024-03-24 11:44:15 -0400

    update 1

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 
-let buttons = document.querySelector(".head-button")
+let buttons = document.querySelector(".head-button");
+let count = 0;
 
-buttons.addEventListener
+buttons.addEventListener("click", increment());
+console.log("hello world");
+
+document.getElementById("click-count").textContent = "new teext";
+
+function increment(){
+    document.getElementById("click-count").textContent = count.toString();
+    console.log(count);
+    count++;
+}
